591f46f67cc62abf7a522eeefdf1a5f8191a2e7b
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-02-14 15:12:37 +0200

    Initial commit

---
+++ src/main/java/org/cyberrealm/tech/bazario/backend/BazarioBackendApplication.java
@@ -0,0 +1,13 @@
+package org.cyberrealm.tech.bazario.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazarioBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BazarioBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cyberrealm/tech/bazario/backend/BazarioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cyberrealm.tech.bazario.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazarioBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
