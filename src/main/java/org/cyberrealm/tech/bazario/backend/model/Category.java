4e56d92b2145e91a89bb167bdbb69aef4de0570d
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-02-21 16:21:50 +0200

    Added entity Category

---
+++ src/main/java/org/cyberrealm/tech/bazario/backend/model/Category.java
@@ -0,0 +1,21 @@
+package org.cyberrealm.tech.bazario.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE categories SET is_deleted = true WHERE id=?")
+@SQLRestriction("is_deleted=false")
+@Table(name = "categories")
+public class Category {
+    @Id
+    private Long id;
+    private String categoryName;
+}
