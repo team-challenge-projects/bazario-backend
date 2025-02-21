c98cace64cc95b50756319b50af35b597cf24996
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-02-21 16:29:09 +0200

    Added entity PasswordResetToken

---
+++ src/main/java/org/cyberrealm/tech/bazario/backend/model/PasswordResetToken.java
@@ -0,0 +1,28 @@
+package org.cyberrealm.tech.bazario.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "password_reset_tokens")
+public class PasswordResetToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    private LocalDateTime expiryDate;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
