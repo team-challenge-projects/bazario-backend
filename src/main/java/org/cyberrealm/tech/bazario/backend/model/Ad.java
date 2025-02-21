b0382641d58ea78cd1aa2e0a80f2dd9597d2b955
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-02-21 16:21:27 +0200

    Added entity Ad

---
+++ src/main/java/org/cyberrealm/tech/bazario/backend/model/Ad.java
@@ -0,0 +1,43 @@
+package org.cyberrealm.tech.bazario.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE ads SET is_deleted = true WHERE id=?")
+@SQLRestriction("is_deleted=false")
+@Table(name = "ads")
+public class Ad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String imageUrl;
+    private BigDecimal price;
+    private Integer rating;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    private String city;
+    private LocalDate publicationDate;
+    @ManyToMany
+    private Set<Category> categories = new HashSet<>();
+}
