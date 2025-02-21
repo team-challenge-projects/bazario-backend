0d07153bcbb575d163ae543befdd5cd2c0c2c7bf
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-02-21 16:39:15 +0200

    Added AuthenticationService

---
+++ src/main/java/org/cyberrealm/tech/bazario/backend/security/AuthenticationService.java
@@ -0,0 +1,24 @@
+package org.cyberrealm.tech.bazario.backend.security;
+
+import lombok.RequiredArgsConstructor;
+import org.cyberrealm.tech.bazario.backend.dto.UserLoginRequestDto;
+import org.cyberrealm.tech.bazario.backend.dto.UserLoginResponseDto;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+
+    public UserLoginResponseDto authenticate(UserLoginRequestDto request) {
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.email(), request.password())
+        );
+        String token = jwtUtil.generateToken(authentication.getName());
+        return new UserLoginResponseDto(token);
+    }
+}
