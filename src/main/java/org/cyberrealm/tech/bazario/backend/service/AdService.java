0938968bbc915c8860f6f265a867908df70043bc
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-08 17:20:33 +0200

    Added AdService

---
+++ src/main/java/org/cyberrealm/tech/bazario/backend/service/AdService.java
@@ -0,0 +1,20 @@
+package org.cyberrealm.tech.bazario.backend.service;
+
+import java.util.List;
+import org.cyberrealm.tech.bazario.backend.dto.ad.AdDto;
+import org.cyberrealm.tech.bazario.backend.dto.ad.CreateAdRequestDto;
+import org.springframework.data.domain.Pageable;
+
+public interface AdService {
+    List<AdDto> findPopular(Pageable pageable);
+
+    AdDto save(CreateAdRequestDto requestDto);
+
+    List<AdDto> findAll(Pageable pageable);
+
+    AdDto findById(Long id);
+
+    AdDto updateById(Long id, CreateAdRequestDto requestDto);
+
+    void deleteById(Long id);
+}
