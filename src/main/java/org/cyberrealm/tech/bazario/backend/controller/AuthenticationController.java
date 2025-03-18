431886daf329216e1c7a485d0bceefc9f2dfc20e
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-18 18:51:42 +0200

    Added refresh and logout endpoints

--- src/main/java/org/cyberrealm/tech/bazario/backend/controller/AuthenticationController.java
+++ src/main/java/org/cyberrealm/tech/bazario/backend/controller/AuthenticationController.java
@@ -2,15 +2,20 @@ package org.cyberrealm.tech.bazario.backend.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.cyberrealm.tech.bazario.backend.dto.AccessTokenResponseDto;
 import org.cyberrealm.tech.bazario.backend.dto.UserLoginRequestDto;
 import org.cyberrealm.tech.bazario.backend.dto.UserLoginResponseDto;
 import org.cyberrealm.tech.bazario.backend.dto.UserRegistrationRequestDto;
 import org.cyberrealm.tech.bazario.backend.dto.UserResponseDto;
 import org.cyberrealm.tech.bazario.backend.exception.RegistrationException;
 import org.cyberrealm.tech.bazario.backend.security.AuthenticationService;
+import org.cyberrealm.tech.bazario.backend.security.CookieService;
 import org.cyberrealm.tech.bazario.backend.service.UserService;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     private final UserService userService;
     private final AuthenticationService authenticationService;
+    private final CookieService cookieService;
 
     @PostMapping("/registration")
     @Operation(summary = "Register a new user",
@@ -37,7 +43,24 @@ public class AuthenticationController {
     @PostMapping("/login")
     @Operation(summary = "Authenticate user",
             description = "Login an existing user by validating their username and password. ")
-    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto request) {
-        return authenticationService.authenticate(request);
+    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto request,
+                                      HttpServletResponse response) {
+        UserLoginResponseDto loginResponse = authenticationService.authenticate(request);
+        cookieService.addRefreshTokenCookie(loginResponse.refreshToken(), response);
+        return loginResponse;
+    }
+
+    @PostMapping("/refresh")
+    @Operation(summary = "Refresh access token",
+            description = "Create new access token by refresh token")
+    public AccessTokenResponseDto refresh(@CookieValue("refreshToken") String refreshToken) {
+        return authenticationService.refreshAccessToken(refreshToken);
+    }
+
+    @GetMapping("/logout")
+    @Operation(summary = "Logout from user account",
+            description = "Clear refresh token cookies")
+    public void logout(HttpServletResponse response) {
+        cookieService.clearRefreshTokenCookie(response);
     }
 }
