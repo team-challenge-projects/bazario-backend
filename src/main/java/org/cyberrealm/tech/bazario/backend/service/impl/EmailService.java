a861bbe9cb8f3a333f7a9b5e3ec8c96850ed00ac
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-21 17:42:19 +0200

    Created EmailService

---
+++ src/main/java/org/cyberrealm/tech/bazario/backend/service/impl/EmailService.java
@@ -0,0 +1,27 @@
+package org.cyberrealm.tech.bazario.backend.service.impl;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String text) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text, true);
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException("Помилка надсилання email: " + e.getMessage());
+        }
+    }
+}
