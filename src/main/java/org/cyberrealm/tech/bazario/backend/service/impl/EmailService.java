5f6cc2e5e453522a537576cd317bbed686588587
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-24 16:58:04 +0200

    Refactored email service

--- src/main/java/org/cyberrealm/tech/bazario/backend/service/impl/EmailService.java
+++ src/main/java/org/cyberrealm/tech/bazario/backend/service/impl/EmailService.java
@@ -3,25 +3,47 @@ package org.cyberrealm.tech.bazario.backend.service.impl;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
-    private final JavaMailSender mailSender;
 
-    public void sendEmail(String to, String subject, String text) {
+    private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    public void sendPasswordResetEmail(String toEmail, String subject,
+                                       int expirationMinutes, String resetLink) {
+        Context context = new Context();
+        context.setVariable("expirationMinutes", expirationMinutes);
+        context.setVariable("resetLink", resetLink);
+
+        String htmlContent = templateEngine.process("reset-password", context);
+
+        sendEmail(toEmail, subject, htmlContent);
+    }
+
+    private void sendEmail(String toEmail, String subject, String htmlContent) {
+        MimeMessage message = mailSender.createMimeMessage();
         try {
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-            helper.setTo(to);
+            MimeMessageHelper helper = new MimeMessageHelper(message, true,
+                    "UTF-8");
+            helper.setFrom(fromEmail);
+            helper.setTo(toEmail);
             helper.setSubject(subject);
-            helper.setText(text, true);
+            helper.setText(htmlContent, true);
             mailSender.send(message);
         } catch (MessagingException e) {
-            throw new RuntimeException("Помилка надсилання email: " + e.getMessage());
+            throw new RuntimeException("Can't send email", e);
         }
     }
 }
+
