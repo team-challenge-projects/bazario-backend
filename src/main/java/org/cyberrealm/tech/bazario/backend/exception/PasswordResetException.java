59eee1f5916207cfeaacf60e7796e397d5585b3e
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-04-04 00:04:10 +0300

    Changed inherited class to abstract

--- src/main/java/org/cyberrealm/tech/bazario/backend/exception/PasswordResetException.java
+++
@@ -1,7 +0,0 @@
-package org.cyberrealm.tech.bazario.backend.exception;
-
-public class PasswordResetException extends RuntimeException {
-    public PasswordResetException(String message) {
-        super(message);
-    }
-}
