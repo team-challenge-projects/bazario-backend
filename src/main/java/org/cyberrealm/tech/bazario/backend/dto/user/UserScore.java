46d8fef675bd6bcf222677726a002ff7bfa12b52
Andrey Sitarskiy <andreysitarskiy09@gmail.com>
2025-07-09 21:51:23 +0300

    Implement ad compare and drop unique phone number

---
+++ src/main/java/org/cyberrealm/tech/bazario/backend/dto/user/UserScore.java
@@ -0,0 +1,4 @@
+package org.cyberrealm.tech.bazario.backend.dto.user;
+
+public record UserScore(Long id, Double score) {
+}
--- src/main/java/org/cyberrealm/tech/bazario/backend/repository/CommentRepository.java
+++ src/main/java/org/cyberrealm/tech/bazario/backend/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package org.cyberrealm.tech.bazario.backend.repository;
 
 import java.util.List;
+import org.cyberrealm.tech.bazario.backend.dto.user.UserScore;
 import org.cyberrealm.tech.bazario.backend.model.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,6 +15,11 @@ public interface CommentRepository extends JpaRepository<Review, Long> {
             SELECT DISTINCT r.evaluated.id FROM Review r
              GROUP BY r.evaluated.id HAVING AVG(r.rating)
              BETWEEN :from AND :to""";
+    String GET_AVG_RATING_BY_USER_IDS = """
+            SELECT new org.cyberrealm.tech.bazario.backend.dto.user.UserScore(
+             r.evaluated.id, AVG(r.rating)) FROM Review r
+             WHERE r.evaluated.id IN (:ids) GROUP BY r.evaluated.id
+            """;
 
     boolean existsByEvaluatorIdAndEvaluatedId(Long currentUserId, Long userId);
 
@@ -22,6 +28,9 @@ public interface CommentRepository extends JpaRepository<Review, Long> {
 
     @Query(FIND_USER_IDS_WITH_AVE_RATING_BETWEEN)
     List<Long> findUserIdsWithAverageRatingBetween(@Param("from") int from, @Param("to") int to);
+
+    @Query(GET_AVG_RATING_BY_USER_IDS)
+    List<UserScore> getAvgRatingByUserIds(@Param("ids") List<Long> ids);
 
     Page<Review> findByEvaluated_Id(Long id, Pageable pageable);
 }
